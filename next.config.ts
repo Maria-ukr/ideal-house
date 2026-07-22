0da3e866dd50ebbff9894d598c149225a8841b53
Maria <mari.mikhaleva.ukr@gmail.com>
2026-07-22 16:42:38 +0300

    setup github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/ideal-house' : '',
+  assetPrefix: isProd ? '/ideal-house/' : '',
 };
 
 export default nextConfig;
