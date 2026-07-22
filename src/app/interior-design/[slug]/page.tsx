94c3efb1d2009d88150e7d6089e700eabd6fc1be
Maria <mari.mikhaleva.ukr@gmail.com>
2026-07-22 18:21:48 +0300

    fix: image path

--- src/app/interior-design/[slug]/page.tsx
+++ src/app/interior-design/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import homeBg from '@/public/images/home-bg.jpg';
 
 export async function generateStaticParams() {
   const projects = [
@@ -15,7 +16,7 @@ async function Page({params}: {params: Promise<{slug: string}>}) {
 
   return (
     <main className="flex flex-col w-full min-h-full m-0">
-        <section className="flex flex-1 min-h-dvh w-full flex-col items-center justify-center home-page__hero relative">
+        <section className="flex flex-1 min-h-dvh w-full flex-col items-center justify-center home-page__hero relative" style={{backgroundImage:`url(${homeBg.src})`}}>
           <div className="container w-full md:w-3/4 lg:w-1/2">
             <h1 className='mb-4 uppercase text-6xl max-md:text-xl max-md:leading-7 leading-14 tracking-tight text-center'>Nested page - {slug} - in Interior design</h1>
             <p className='text-sm leading-4 text-center in-brackets'>We see your entire home as an ecosystem where everything is connected and flows into each other in a harmony of perfect movements. Taking this approach, we create unique solutions through a wide variety of compositions, materials and take care to customize every detail to make your home as unique as you are.</p>
